96082d98c0c87ac3ef5589e67e76218b82e6ff67
Rumi-Shaikh <rumanashaikh986@gmail.com>
2025-08-21 09:51:40 +0530

    Add files via upload

---
+++ int_char_float.java
@@ -0,0 +1,13 @@
+public class int_char_float {
+    public static void main(String[] args) {
+        
+        int integer = 7;
+        char character = 'Z';
+        float floats = 7.77F;
+
+        System.out.println("Integer:"+integer);
+        System.out.println("Character:"+character);
+        System.out.println("Float:"+floats);
+    }
+    
+}
---
+++ user_mssg.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class user_mssg {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your message:");
+
+        String message = sc.nextLine();
+
+        System.out.println("Message:"+message);
+
+
+    }
+    
+}
